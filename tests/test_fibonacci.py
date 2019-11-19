22716daad36884dbd66cb7d2b165d5d500576b42
Shivani Yadav <yadashiv@sheridancollege.ca>
2019-11-19 06:13:38 +0530

    created test to check fibonacci of 4

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -28,3 +28,7 @@ def test_3_for_1():
 def test_4_for_2():
     """Test to check what value is returned for 2"""
     assert [fib for fib in Fibonacci(2)] == [0, 1, 1]
+
+def test_5_for_4():
+    """Test to check what value is returned for 4"""
+    assert [fib for fib in Fibonacci(4)] == [0, 1, 1, 2, 3]
